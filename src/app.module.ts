b9043921ac4950a0e711f895b043d99c5f8b0535
cone26 <sight41siyah@gmail.com>
2023-07-31 21:44:31 +0900

    fix: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,21 +7,22 @@ import { ConfigModule } from '@nestjs/config';
 import { defaultTypeOrmOptions } from './blog/libs/common/src/database/typeorm/typeorm-module.options';
 import { PostController } from './blog/api/board/post.controller';
 import { PostService } from './blog/api/board/post.service';
-import { ContentModule } from './blog/libs/dao/src/post/content.module';
+import { ContentModule } from './blog/libs/dao/src/content/content.module';
 import { CategoryModule } from './blog/libs/dao/src/category/category.module';
 import { CategoryController } from './blog/api/category/category.controller';
 import { CategoryService } from './blog/api/category/category.service';
+import { TypeOrmExModule } from './blog/libs/common/src/database/typeorm/typeorm-ex.module';
 
 @Module({
   imports: [
     //config
-    // Configuration,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '/.env',
-    }),
+    Configuration,
+    // ConfigModule.forRoot({
+    //   isGlobal: true,
+    //   envFilePath: './config/.test.env',
+    // }),
     // DB
-    TypeOrmModule.forRoot(defaultTypeOrmOptions),
+    TypeOrmExModule.forRoot(defaultTypeOrmOptions),
     ContentModule,
     CategoryModule,
   ],
